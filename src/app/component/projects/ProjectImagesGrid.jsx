3e649c4c0ec8c2ca6360780f7a8fb2f90739b0ee
AbdallaMon <abdotlos60@gmail.com>
2025-11-25 14:23:34 +0200

    Fix image width in mobile

--- src/app/component/projects/ProjectImagesGrid.jsx
+++ src/app/component/projects/ProjectImagesGrid.jsx
@@ -80,6 +80,7 @@ export default function ProjectImagesGrid({ project, lng = "ar" }) {
               display: "flex",
               flexDirection: "column",
               gap: 2,
+              width: "100%",
             }}
           >
             {columnImages.map((img) => (
