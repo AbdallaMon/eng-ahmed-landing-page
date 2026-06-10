721a8561f1685c098c01f5bf754765f09771a6c4
Abdalla <abdalle.webdev@gmail.com>
2026-06-10 19:15:18 +0300

    change the ui

--- src/app/data/mainPage.js
+++ src/app/data/mainPage.js
@@ -3,18 +3,21 @@ export const arMainPageData = [
     title: "من هو المهندس أحمد؟",
     // subTitle: "وهذا السبب اللي خلّاني أبدأ",
     href: "/about",
+    // href: process.env.NEXT_PUBLIC_REGISTER_URL,
     image: "/main-page-hero.png",
   },
   {
-    title: "استشارة",
+    title: "طلب استشارة",
     href: "https://decorstores.ltd/en/products/consultation-with-engineer-ahmed",
     image: "/consultation.jpg",
     type: "COVER",
   },
   {
-    title: "الدائرة المقربة",
-    href: "whatsapp://channel/0029Vb6IrAgFy72LBOHa7t3E",
-    image: "/whats-app-icon.png",
+    title: "طلب تصميم",
+    // href: "whatsapp://channel/0029Vb6IrAgFy72LBOHa7t3E",
+    // image: "/whats-app-icon.png",
+    image: "/projects/project-2.png",
+    href: process.env.NEXT_PUBLIC_REGISTER_URL,
   },
 ];
 
@@ -22,18 +25,22 @@ export const enMainPageData = [
   {
     title: "Who is Engineer Ahmed?",
     // subTitle: "And that's the reason I started",
-    href: "/about",
+    // href: process.env.NEXT_PUBLIC_REGISTER_URL,
     image: "/main-page-hero.png",
+    href: "/about",
   },
   {
-    title: "Consultation",
+    title: "Consultation with Engineer Ahmed",
     href: "https://decorstores.ltd/en/products/consultation-with-engineer-ahmed",
     image: "/consultation.jpg",
     type: "COVER",
   },
   {
-    title: "The Inner Circle",
-    href: "whatsapp://channel/0029Vb6IrAgFy72LBOHa7t3E",
-    image: "/whats-app-icon.png",
+    title: "Design request",
+    // href: "whatsapp://channel/0029Vb6IrAgFy72LBOHa7t3E",
+    // image: "/whats-app-icon.png",
+    image: "/projects/project-2.png",
+    href: process.env.NEXT_PUBLIC_REGISTER_URL,
+    // href: "/projects",
   },
 ];
